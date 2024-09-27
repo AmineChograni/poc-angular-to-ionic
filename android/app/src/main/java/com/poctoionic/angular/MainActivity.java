290d7d557d31e694ae55ee24f4ffe7f547e69101
Amine CHOGRANI <aminechograni@gmail.com>
2024-09-27 10:08:02 +0100

    creation android app

---
+++ android/app/src/main/java/com/poctoionic/angular/MainActivity.java
@@ -0,0 +1,5 @@
+package com.poctoionic.angular;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.poctoionic.angular',
+  appName: 'poc-angular-to-ionic',
+  webDir: 'dist/poc-angular-to-ionic/browser',
+  bundledWebRuntime: false
+};
+
+export default config;
